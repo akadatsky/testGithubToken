f1738e664982a6bc779ce559ce489a114e076e23
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2021-03-18 11:48:01 +0200

    Initial commit

---
+++ src/com/company/IntList.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class IntList {
+
+    private int[] arr = new int[0];
+
+    public void add(int value) {
+        // TODO implement
+    }
+
+    public int get(int index) {
+        // TODO implement
+        return 0;
+    }
+
+    public void set(int index, int value) {
+        // TODO implement
+    }
+
+    public int getSize() {
+        // TODO implement
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        // TODO implement
+        return "";
+    }
+}
